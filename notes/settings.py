fbbe39c5a3898ad7ed5140e0255b97e164119cf4
Kunal Raj Pal <kunalrajpal03@gmail.com>
2025-10-06 07:50:39 +0530

    Prepared Settings for Render deployment

--- notes/settings.py
+++ notes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j($+)l@fa@ua269s60k(wofuagbe+$pvf2_^=y9zmi7b0sm39l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
